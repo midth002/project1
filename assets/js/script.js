d9ca7f06d327b9540914e56956bf692b6c347dc2
Andrew Midthun <midth002@gmail.com>
2022-04-04 13:54:05 -0500

    DOM to the brewData div and entered in some list items from our api.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,35 @@
+var brewData = $('#brewData')
+
+
+
+
 function getApi(city) {
 
-    var url = "https://api.openbrewerydb.org/breweries?by_city=" + city
+    var url = "https://api.openbrewerydb.org/breweries?per_page=1&by_city=" + city
     fetch(url)
         .then(function (response) {
             return response.json();
         })  
         .then(function (data) { 
-            console.log(data)
-            console.log(url)
+            for (i=0; i < data.length; i++) {
+
+                var ul = $('<ul>')
+                var li1 = $('<li>')
+                var li2 = $('<li>')
+                var li3 = $('<li>')
+                var li4 = $('<li>')
+
+                li1.text(data[i].name)
+                li1.text(data[i].brewery_type)
+                li1.text(data[i].street)
+                li1.text(data[i].website_url)
+            }
         })
     
 }
+
+
+
 
 
 getApi("Minneapolis")
